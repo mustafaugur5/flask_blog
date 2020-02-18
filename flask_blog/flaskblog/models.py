1ad74281c5dfd9f32a2d65e990b9f3741e9d3166
mustafaugur5 <56108027+mustafaugur5@users.noreply.github.com>
2020-02-18 13:01:26 +0300

    Add files via upload

---
+++ flask_blog/flaskblog/models.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from flaskblog import db, login_manager
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    feed_period = db.Column(db.Integer, nullable=False, default=6)       
+    feed_amount = db.Column(db.Integer, nullable=False, default=50)      
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}','{self.feed_period}', '{self.feed_amount}')"
+       
+
+#I have added feed_amount to database for post. Use this for creating the content for the Post
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    # feed_amount = db.Column(db.Integer, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
