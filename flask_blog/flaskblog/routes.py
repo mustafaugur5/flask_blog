91484910dfc1e946617fc49a38bc07954a6d23e8
mustafaugur5 <56108027+mustafaugur5@users.noreply.github.com>
2020-02-24 21:48:53 +0300

    Update routes.py

--- flask_blog/flaskblog/routes.py
+++ flask_blog/flaskblog/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from datetime import datetime
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
